cde08cb036d58531ca79139e96189163a7135925
Nootey33 <poiskusni@gmail.com>
2024-07-05 11:44:43 +0200

    temporary projects carusel commit

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -32,7 +32,38 @@
         </div>
 
         <div class="main-col col w-50">
-          <img src="@/assets/images/coding.png" style="width: 100%; height: auto;"/>
+
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active" style="background: none;">
+                <img class="d-block w-100" src="@/assets/images/close-up-laptop.jpg" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>...</h5>
+                  <p>...</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="@/assets/images/close-up-laptop.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="@/assets/images/close-up-laptop.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
         </div>
 
       </div>
